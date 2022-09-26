3dccf1eb3c50293fb0986928a728214b61d5adb8
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-09-26 17:26:44 -0400

    setting up hardhat env. Have to delay, as Alchemy is completely down.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {},
+        goerli: {},
+    },
+    solidity: "0.8.8",
 };
