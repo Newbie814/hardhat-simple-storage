c764afff332ddd003166964eedf3207599a567a8
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-10-01 00:08:27 -0400

    programmed verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat");
+const { isCommunityResourcable } = require("@ethersproject/providers");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +9,27 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log(`Deployed to ${simpleStorage.address}`);
+    console.log(network.config);
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.includes("Contract source code already verified")) {
+            console.log("Contract already verified");
+        } else {
+            console.log(error.message);
+        }
+    }
 }
 
 main()
