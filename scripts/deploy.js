83942d329cef3133ca71da9ebf638dac89319766
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-10-01 18:41:49 -0400

    testing verify program and scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,9 +11,18 @@ async function main() {
     console.log(`Deployed to ${simpleStorage.address}`);
     console.log(network.config);
     if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for txs...");
         await simpleStorage.deployTransaction.wait(6);
         await verify(simpleStorage.address, []);
     }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current value is: ${currentValue}`);
+
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated value is: ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {
---
+++ tasks/block-number.js
@@ -0,0 +1,9 @@
+import {task} = require("hardhat/config");
+
+
+task("block-number", "Prints the current block number").setAction(
+  async (taskArgs, hre) => {
+    const blockNumber = await hre.ethers.provider.getBlockNumber();
+    console.log(blockNumber);
+  }
+);
