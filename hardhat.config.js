45389db652255ef29aa7c74a1abbcb5368b43687
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-09-30 23:20:07 -0400

    connected to goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
-        rinkeby: {},
-        goerli: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+            chainId: 5,
+        },
     },
     solidity: "0.8.8",
 };
