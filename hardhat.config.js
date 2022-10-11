e09a727b5db5cfc3373dd3574a11dd22bb7bdcad
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-10-10 20:55:29 -0400

    setting up tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x";
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -25,5 +28,12 @@ module.exports = {
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 };
